09730e22488d30e8dbacf6f6448c12c926aac726
jimmy-guzman <hi@jimmyguzman.com>
2021-01-18 22:21:50 -0600

    chore: 🎸 add `--commitlint` flag to leverage commitlint config

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,14 +2,16 @@ import { GitzyConfig } from '../interfaces'
 import { validateUserConfig } from '../utils'
 import { getCommitlintConfig, loadConfig } from './helpers'
 
-export const getUserConfig = async (): Promise<GitzyConfig | null> => {
+export const getUserConfig = async (
+  commitlint?: boolean
+): Promise<GitzyConfig | null> => {
   const loaded = await loadConfig<GitzyConfig>('gitzy')
 
   if (loaded) {
     const isValid = await validateUserConfig(loaded.config)
 
     if (isValid) {
-      if (loaded.config.useCommitlintConfig) {
+      if (commitlint || loaded.config.useCommitlintConfig) {
         const commitlintConfig = await getCommitlintConfig()
 
         return { ...loaded.config, ...commitlintConfig }
