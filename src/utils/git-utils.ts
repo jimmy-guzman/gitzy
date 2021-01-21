ea7e283973ac7f2d35857889d3fc837759b5a2d5
jimmy-guzman <hi@jimmyguzman.com>
2021-01-20 20:47:45 -0600

    feat(cli): 🎸 checks if current directory is a git repo

--- src/utils/git-utils.ts
+++ src/utils/git-utils.ts
@@ -1,3 +1,43 @@
-export const shouldCheckIfStaged = (array: string[] = []): boolean => {
+import { exec } from 'child_process'
+
+import { hint } from './logging'
+
+/**
+ * Determines wether or not files are staged.
+ */
+export const checkIfStaged = (): Promise<string> => {
+  return new Promise((resolve, reject) => {
+    exec('git --no-pager diff --cached --quiet --exit-code', error => {
+      if (error) {
+        resolve('')
+      }
+      reject(
+        new Error(
+          `No files staged \n${hint(
+            'You can use "gitzy -p -a" to replicate git -am'
+          )}`
+        )
+      )
+    })
+  })
+}
+
+export const checkIfGitRepo = (): Promise<string> => {
+  return new Promise((resolve, reject) => {
+    exec('git rev-parse --is-inside-work-tree', error => {
+      if (error) {
+        reject(
+          new Error(
+            `Not a git repository \n${hint('You can try running "git init"')}`
+          )
+        )
+      }
+
+      resolve('')
+    })
+  })
+}
+
+export const shouldDoGitChecks = (array: string[] = []): boolean => {
   return !['--add', '-a', '--amend'].some(flag => array.includes(flag))
 }
--- src/utils/logging/logging.ts
+++ src/utils/logging/logging.ts
@@ -1,7 +1,7 @@
-import { blue, red } from 'ansi-colors'
+import { blue, red, yellow } from 'ansi-colors'
 
 export const info = (message: string): string => blue(`❯ ${message}`)
-
+export const hint = (message: string): string => yellow(`❯ ${message}`)
 export const danger = (message: string): string => red(`❯ ${message}`)
 
 // eslint-disable-next-line no-console
