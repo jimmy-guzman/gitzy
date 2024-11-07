d8c2f0d8ac500fb662a305c5daa10c520b62d319
jimmy-guzman <hi@jimmyguzman.com>
2024-11-06 18:46:53 -0600

    feat(*): ✨ add support for node v22

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,5 @@
 import jimmyDotCodes from "@jimmy.codes/eslint-config";
 
-export default jimmyDotCodes(
-  {
-    typescript: { project: ["./tsconfig.json", "./e2e/tsconfig.json"] },
-  },
-  {
-    files: [`**/*.test.?([cm])[jt]s?(x)`],
-    rules: {
-      "@typescript-eslint/no-unsafe-argument": "off",
-      "@typescript-eslint/no-unsafe-assignment": "off",
-    },
-  },
-);
+export default jimmyDotCodes({
+  typescript: { project: ["./tsconfig.json", "./e2e/tsconfig.json"] },
+});
