17c78e8eed9d200fdb3bc5c7506b93b50ccc4445
Jimmy Guzman <30631540+jimmy-guzman@users.noreply.github.com>
2025-10-27 18:09:10 -0500

    fix(*): 🐛 actually support `.mjs` extension in config files (#589)

--- src/config/loaders/loader.spec.ts
+++ src/config/loaders/loader.spec.ts
@@ -3,26 +3,6 @@ import { writeFile } from "node:fs/promises";
 
 import { getSearchPlaces, loadConfig } from "./loader";
 
-const expectedSearchPlaces = [
-  "package.json",
-  ".gitzyrc",
-  ".gitzyrc.json",
-  ".gitzyrc.yaml",
-  ".gitzyrc.yml",
-  ".gitzyrc.js",
-  ".gitzyrc.cjs",
-  "gitzy.config.js",
-  "gitzy.config.cjs",
-  ".config/.gitzyrc",
-  ".config/.gitzyrc.json",
-  ".config/.gitzyrc.yaml",
-  ".config/.gitzyrc.yml",
-  ".config/.gitzyrc.js",
-  ".config/.gitzyrc.cjs",
-  ".config/gitzy.config.js",
-  ".config/gitzy.config.cjs",
-];
-
 describe("searchPlaces", () => {
   afterEach(() => {
     access("./.arc.json", (error) => {
@@ -38,7 +18,31 @@ describe("searchPlaces", () => {
   });
 
   it("should return all search places", () => {
-    expect(getSearchPlaces("gitzy")).toStrictEqual(expectedSearchPlaces);
+    expect(getSearchPlaces("gitzy")).toMatchInlineSnapshot(`
+      [
+        "package.json",
+        ".gitzyrc",
+        ".gitzyrc.json",
+        ".gitzyrc.yaml",
+        ".gitzyrc.yml",
+        ".gitzyrc.js",
+        ".gitzyrc.cjs",
+        ".gitzyrc.mjs",
+        "gitzy.config.js",
+        "gitzy.config.cjs",
+        "gitzy.config.mjs",
+        ".config/.gitzyrc",
+        ".config/.gitzyrc.json",
+        ".config/.gitzyrc.yaml",
+        ".config/.gitzyrc.yml",
+        ".config/.gitzyrc.js",
+        ".config/.gitzyrc.cjs",
+        ".config/.gitzyrc.mjs",
+        ".config/gitzy.config.js",
+        ".config/gitzy.config.cjs",
+        ".config/gitzy.config.mjs",
+      ]
+    `);
   });
 
   it("should return null if no config is found", async () => {
