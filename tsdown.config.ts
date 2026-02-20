1a238979870933a11ed0239a8da4210101bf27f0
Jimmy Guzman <30631540+jimmy-guzman@users.noreply.github.com>
2026-02-19 19:17:50 -0600

    fix(deps): 🐛 bump `yaml` to v2.8.2 (#642)

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -2,6 +2,14 @@ import { defineConfig } from "tsdown";
 
 export default defineConfig({
   entry: "src/run.ts",
+  inlineOnly: [
+    "commander",
+    "ansi-colors",
+    "enquirer",
+    "valibot",
+    "@leeoniya/ufuzzy",
+    "yaml",
+  ],
   minify: true,
   publint: true,
 });
